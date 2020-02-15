f0387656102077f4167211a3a26746260ceb6d9c
Captainz <1533519929@qq.com>
2020-02-15 16:43:58 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Captain Z on 2020/2/15.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int cityId;
+    private int weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+}
